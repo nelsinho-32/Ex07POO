679f0f07878ce272e83b38478840f1633bea3085
nelsinho-32 <110434694+nelsinho-32@users.noreply.github.com>
2025-10-30 11:59:23 -0300

    Add files via upload

---
+++ Q8.java
@@ -0,0 +1,15 @@
+public class Q8 {
+    public static void main(String[] args) {
+    int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int[] B = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int[] C = new int[A.length];
+
+    for(int i = 0; i < B.length; i++) {
+        C[i] = A[i] * B[i];
+        System.out.println(A[i]);
+        System.out.println(B[i]);
+        System.out.println(C[i]);
+        System.out.println("=========");
+    }
+    }
+}
